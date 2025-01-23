27b48667463967f039481d8629152ceaba9cc41e
LopsidedMango <tomomatson@icloud.com>
2025-01-23 11:15:18 +0000

    Added output file creation, BMP and DIB header reading and writing to new file

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 typedef struct {
@@ -9,6 +10,20 @@ typedef struct {
     unsigned short reserve2;
     unsigned int offset;
 } BMPheader;
+
+typedef struct {
+    unsigned int size;
+    int width; // width can be negative
+    int height;
+    unsigned short planes;
+    unsigned short bitCount;
+    unsigned int compression;
+    unsigned int imageSize;
+    int hRez; // horizontal resolution
+    int vRex; // vertical resolution
+    unsigned int colourCount;
+    unsigned int impColourCount;
+} DIBheader;
 
 void grayscale(const char* inputFile){
     FILE* inputBMP = fopen(inputFile, "rb");
@@ -25,6 +40,30 @@ void grayscale(const char* inputFile){
         fclose(inputBMP);
         exit(EXIT_FAILURE);
     }
+    DIBheader dibHeader;
+    fread(&dibHeader, sizeof(dibHeader), 1, inputBMP);
+
+    // Check we are in 24 bit
+    if (dibHeader.bitCount != 24){
+        perror("24 bit BMP files are only supported at the moment :(");
+        fclose(inputBMP);
+        exit(EXIT_FAILURE);
+    }
+    // Create the output file
+    char* outputFileName = (char*)malloc(strlen(inputFile) + 11); // size of inputfile + "greyscale" + \0
+    strncpy(outputFileName, inputFile, (strlen(inputFile) - 4));
+    strcat(outputFileName, "_greyscale.bmp\0");
+    
+    FILE* outputBMP = fopen(outputFileName, "wb");
+    fwrite(&bmpHeader, sizeof(bmpHeader), 1, outputBMP);
+    fwrite(&dibHeader, sizeof(dibHeader), 1, outputBMP);
+
+    // Move the file pointer to the correct location to begin writing
+    fseek(inputBMP, bmpHeader.offset, SEEK_SET);
+    fseek(outputBMP, bmpHeader.offset, SEEK_SET);
+    
+
+
 
 
 
@@ -33,11 +72,11 @@ void grayscale(const char* inputFile){
 
 
 int main(int argc, char* argv[]){
-    printf("Hello world!");
+
     if (argc != 2){
         fprintf(stderr, "Usage: %s <input .bmp file>", argv[0]);
     }
-    grayscale(argv[0]);
+    grayscale(argv[1]);
     return EXIT_SUCCESS;
 
 }
