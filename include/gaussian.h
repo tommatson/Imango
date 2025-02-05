803914710856b5be4b9ec5a25e916414df84014f
LopsidedMango <tomomatson@icloud.com>
2025-02-05 20:02:45 +0000

    Implemented seperate header file for gaussian and freed some memory

---
+++ include/gaussian.h
@@ -0,0 +1,3 @@
+double approximateExponential(double squaredPart);
+double calculateKernelItem(float stanDev, int i, int kernelWidth);
+void gaussianConvert(const char* inputFile, int kernelWidth, float stanDev);
